8fa6ae8d643b3fca89d36433dc63ef07c7c83f52
pinanina <dyang01@mica.edu>
2020-07-10 21:04:30 +0900

    ?

---
+++ 프로젝트/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return 'This is Home!'
+
+@app.route('/mypage')
+def mypage():  
+   return 'This is My Page!'
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0',port=5000,debug=True)
