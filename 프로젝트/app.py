4d62ce16bd7aa61f9c40444cf9be724f8c807fd5
pinanina <dyang01@mica.edu>
2020-07-14 17:23:01 +0900

    css 없어서 실행안되는 설문

--- 프로젝트/app.py
+++ 프로젝트/app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'This is Home!'
+   return render_template('index.html')
 
-@app.route('/mypage')
-def mypage():  
-   return 'This is My Page!'
+@app.route('/test')
+def test():
+   return render_template('test.html')
+
+@app.route('/result')
+def result():
+   return render_template('result.html')
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
